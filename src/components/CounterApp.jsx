eb08a960908e98c7abfdfa04f40ba1d5ae8b3169
Juan Colin <juandiegopenaceferino@gmail.com>
2024-06-21 15:55:54 -0500

    Hicimos este repositorio para guardar 3 Customs hooks para los proyectos finales

---
+++ src/components/CounterApp.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useCounter } from '../hooks/useCounter'
+
+export const CounterApp = () => {
+
+    const { counter, increment, reset, decrement } = useCounter(0)
+
+
+
+  return (
+    <>
+    <h1>Counter</h1>
+    <h4>{counter}</h4>
+    <button className="btn btn-outline-primary" onClick={()=>increment()}>+1</button>
+    <button className="btn btn-outline-primary" onClick={()=>reset()}>Reset</button>
+    <button className="btn btn-outline-primary" onClick={()=>decrement(1, false)}>-1</button>
+    </>
+  )
+}
